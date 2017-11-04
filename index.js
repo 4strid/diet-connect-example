a71a061bd239f17617f7cf9105176b2ee0790cde
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2017-11-03 18:55:38 -0700

    update

--- index.js
+++ index.js
@@ -2,51 +2,47 @@ var path = require('path')
 var server = require('diet')
 var compatible = require('diet-connect');
 
+
 // random assortment of middleware
 var logger = require('morgan');
 var createStatic = require('connect-static'); // also works with express-static
 var session = require('express-session');
-var SessionStore = require('express-nedb-session')(session);
 
-function main () {
-    var app = server()
-	app.listen('http://localhost:8080')
+var app = server()
+app.listen('http://localhost:8080')
 
-	app.header(compatible(logger('dev')));
+app.header(compatible(logger('dev')));
 
-	app.header(compatible(session({
-		secret: 'mysecretkey',
-		resave: false,
-		saveUninitialized: false,
-		cookie: {
-			maxAge: (365 * 24 * 3600 * 1000) / 2
-		},
-		store: new SessionStore({filename: path.join(app.path, 'db','session.db')})
-	})));
+app.header(compatible(session({
+	secret: 'mysecretkey',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		maxAge: (10 * 60 * 1000) / 2
+	}
+})));
 
-	// static middleware
-	createStatic({
-		dir: 'static',
-		aliases: [['/','/index.html']]
-	}, function (err, static) {
-		app.footer(compatible(static));
-	});
+// static middleware
+createStatic({
+	dir: 'static',
+	aliases: [['/','/index.html']]
+}, function (err, static) {
+	app.footer(compatible(static));
+});
 
-	// (taken from express session examples)
-	// refresh the page to see the counter go up
-	app.get('/counter', function ($) {
-		if ($.session.views) {
-			$.session.views++;
-			$.end('you have viewed this page ' + $.session.views + ' times')
-		} else {
-			$.session.views = 1;
-			$.end('you are viewing this page for the first time');
-		}
-	});
-}
+// (taken from express session examples)
+// refresh the page to see the counter go up
+app.get('/counter', function ($) {
+	if ($.session.views) {
+		$.session.views++;
+		$.end('you have viewed this page ' + $.session.views + ' times')
+	} else {
+		$.session.views = 1;
+		$.end('you are viewing this page for the first time');
+	}
+});
 
-module.exports = main
-
-if (require.main === module) {
-	main()
-}
+app.get('/destroy', function ($) {
+	$.session.destroy()
+	$.end('counter is reset')
+});
